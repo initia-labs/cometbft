9128e8447bb7a196231afdd0cb82bd3e46980df6
beer-1 <147697694+beer-1@users.noreply.github.com>
2024-07-18 17:26:39 +0900

    limit block range (#4)

--- rpc/core/block_bulk.go
+++ rpc/core/block_bulk.go
@@ -7,8 +7,18 @@ import (
 	rpctypes "github.com/cometbft/cometbft/rpc/jsonrpc/types"
 )
 
+// hard limit to the number of blocks that can be queried at once
+const MaxBlockRange = 100
+
 // BlockBulk return invalid block information
 func (env *Environment) BlockBulk(ctx *rpctypes.Context, start, end int64) (*ctypes.ResultBlockBulk, error) {
+	if start > end {
+		return nil, fmt.Errorf("start height is greater than end height")
+	}
+	if end-start >= MaxBlockRange {
+		return nil, fmt.Errorf("block range exceeds maximum limit %d", MaxBlockRange)
+	}
+
 	blocks := make([][]byte, end-start+1)
 	for i := start; i <= end; i++ {
 		bz := env.BlockStore.LoadBlockBytes(i)
