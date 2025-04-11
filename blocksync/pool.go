956fe82acaafc0057751d43efab13740f555a748
beer-1 <beer-1@codingcats.xyz>
2025-04-12 01:30:41 +0900

    increase p2p timeout to prevent connection reset

--- blocksync/pool.go
+++ blocksync/pool.go
@@ -55,7 +55,7 @@ const (
 	minBlocksForSingleRequest = 50
 )
 
-var peerTimeout = 15 * time.Second // not const so we can override with tests
+var peerTimeout = 60 * time.Second // not const so we can override with tests
 
 /*
 	Peers self report their heights when we join the block pool.
