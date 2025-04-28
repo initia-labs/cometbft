cc2f9db6ce4e5b4ca9d4d82a83e1c4b29a9fb1d1
sh-cha <snghuncha@gmail.com>
2025-04-28 17:03:01 +0900

    parse config when starting command

--- cmd/cometbft/commands/reindex_event.go
+++ cmd/cometbft/commands/reindex_event.go
@@ -54,6 +54,11 @@ want to use this command.
 	cometbft reindex-event --start-height 2 --end-height 10
 	`,
 	Run: func(cmd *cobra.Command, args []string) {
+		config, err := ParseConfig(cmd)
+		if err != nil {
+			fmt.Println(reindexFailed, err)
+			return
+		}
 		bs, ss, err := loadStateAndBlockStore(config)
 		if err != nil {
 			fmt.Println(reindexFailed, err)
