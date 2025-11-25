380221f4267be940510cabd06eda2537c6b62587
beer-1 <147697694+beer-1@users.noreply.github.com>
2025-11-25 16:25:11 +0900

    fix: use pre-set proposer (#59)
    * use pre-set proposer
    
    * update proposer if not set

--- types/validator_set.go
+++ types/validator_set.go
@@ -341,7 +341,10 @@ func (vals *ValidatorSet) GetProposer() (proposer *Validator) {
 	if len(vals.Validators) == 0 {
 		return nil
 	}
-	return vals.findProposer()
+	if vals.Proposer == nil {
+		vals.Proposer = vals.findProposer()
+	}
+	return vals.Proposer.Copy()
 }
 
 func (vals *ValidatorSet) findProposer() *Validator {
